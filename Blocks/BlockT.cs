89c7f2fca05f0dd96bbff7e1b7afa94f9684d117
Itstritix <jiabao.tsui@gmail.com>
2025-01-12 14:23:04 +0100

    edit BlockT class

--- Blocks/BlockT.cs
+++ Blocks/BlockT.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace Tetris.Blocks
 {
-    internal class BlockT
+    public class BlockT : Block
     {
+        public override int Type => 2;
+        Position[][] tilesPosition = new Position[][]
+        {
+            new Position[] {new (0, 0), new (1, 0), new (1, 1), new (1, 2)},
+            new Position[] {new (0, 1), new (0, 2), new (1, 1), new (2, 1)},
+            new Position[] {new (1, 0), new (1, 1), new (1, 2), new (2, 2)},
+            new Position[] {new (0, 1), new (1, 1), new (2, 0), new (2, 1)}
+        };
+
+        protected override Position[][] Tiles => tilesPosition;
+        protected override Position StartingPosition => new Position(0, 3);
     }
 }
